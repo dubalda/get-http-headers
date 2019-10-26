5878fcba82142b7b12e0437c06d3bc1d5a75f335
ls <ls@omikron.pro>
2019-10-26 20:49:18 +0300

    test

--- python/README.md
+++ python/README.md
@@ -1,4 +1,4 @@
-docker build -f Dockerfile.python -t http-headers-python . && docker run --rm -p 5002:5002 --name http-headers-python http-headers-python
+docker build -f Dockerfile.python -t http-headers-python . && docker run -it --rm -p 5002:5002 --name http-headers-python http-headers-python
 
 docker run -it --rm --name http-headers-python -v "$PWD/python":/usr/src/myapp -w /usr/src/myapp -p 5002:5002 python:3 python webserver.py
 
--- python/webserver.py
+++ python/webserver.py
@@ -4,14 +4,25 @@ import logging
 
 PORT = 5002
 
+f= open("index.html","w+")
+f.write("<h1>HTTP headers</h1>")
+f.close()
+
 class GetHandler(SimpleHTTPRequestHandler):
-
     def do_GET(self):
+        if self.path == '/':
+          self.path = '/index.html'
         logging.error(self.headers)
+        print('TEST User-Agent: ' + self.headers.get('User-Agent'))
+        f = open("index.html", "a+")
+        for key in self.headers.keys():
+            print(key)
+            f.write('<br><b>' + key + ':</b> ' + self.headers.get(key))
+        f.write('<br><br>')
+        f.close()
         SimpleHTTPRequestHandler.do_GET(self)
-
 
 Handler = GetHandler
 httpd = TCPServer(("", PORT), Handler)
-
+print("Started httpserver on port ", PORT)
 httpd.serve_forever()
